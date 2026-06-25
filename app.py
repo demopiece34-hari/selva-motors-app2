cb8d07866401886da331ff336e007db311f991fe
demopiece34-hari <demopiece34@gmail.com>
2026-06-25 23:49:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import zipfile
 import time as time_module
 import json
 import base64
-from pages.hero_bikes_page import page_hero_bikes
+from pages.hero_bikes_catalog_en_th import page_hero_bikes
 from debug.debug_engine import *
 from pathlib import Path
 from datetime import datetime, time
